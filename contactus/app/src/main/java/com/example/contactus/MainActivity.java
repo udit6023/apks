f6341e7de01965ea381f4130537934c62f040098
Udit Soni <uditsoni6023@gmail.com>
2021-10-13 00:02:01 +0530

    push this

---
+++ contactus/app/src/main/java/com/example/contactus/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.contactus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button=findViewById(R.id.button);
+     button.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View view) {
+             Toast.makeText(MainActivity.this, "your response is submitted", Toast.LENGTH_SHORT).show();
+         }
+     });
+    }
+}
